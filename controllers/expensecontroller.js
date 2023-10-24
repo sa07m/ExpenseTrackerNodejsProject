b8f951b39bf955d170d89de077f9c5d6b5ddf9ed
sa07m <127376011+sa07m@users.noreply.github.com>
2023-10-24 23:14:06 +0530

    Pagination 1.12

--- controllers/expensecontroller.js
+++ controllers/expensecontroller.js
@@ -32,16 +32,68 @@ exports.postExpense = async (req,res,next)=>{
     };
 }
 
-exports.getExpense = (req,res,next)=>{
-    console.log('in get expense controller')
+exports.getExpense = (req, res, next) => {
+    const itemsPerPage = 10;
+    const currentPage = parseInt(req.query.page) || 1;
+    const offset = (currentPage - 1) * itemsPerPage;
 
-    Expense.findAll({where : {userId : req.user.id}})
-    .then((expenses)=>{
-        // console.log('fetched' , expenses  );
-        res.json(expenses);
+    Expense.findAndCountAll({
+        where: { userId: req.user.id },
+        offset,
+        limit: itemsPerPage
     })
-    .catch()
+        .then(async result => {
+            const totalItems = result.count;
+            const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+            // const hasNextPage = currentPage < totalPages;
+            // const hasPreviousPage = currentPage > 1;
+
+            res.json({
+                expenses: result.rows,
+                totalPages,
+            });
+        })
+        .catch(err => console.log(err));
 }
+
+
+// exports.getExpense = (req,res,next)=>{
+
+//     const itemsParPage = 2;
+//     //const of = ((req.query.page || 1) - 1);
+//     const currentPage = parseInt(req.query.page) || 1;
+//     console.log(of, '63 PER OFF HAI ');
+//     Expense.findAll({
+//         where: { userId: req.user.id },
+//         offset: of * itemsParPage,
+//         limit: itemsParPage
+//     })
+    
+//         .then(async result => {
+//             let pre; let nex; let prev; let nextv;
+//             if (of === 0) {
+//                 pre = false;
+//             } else {
+//                 pre = true;
+//                 prev = of;
+//             }
+//             const ans = await nextt(req.user.id, (of + 1) * itemsParPage, itemsParPage);
+//             if (ans === true) {
+//                 nex = true;
+//                 nextv = Number(of) + Number(2);
+//             } else {
+//                 nex = false;
+//             }
+//             console.log(prev, 'PREVIOUSBUTTON');
+//             console.log(pre, 'PREBUTTON');
+//             // console.log(nex, 'nexIOUSBUTTON');
+//             // console.log(nextv, 'nextvOUSBUTTON');
+//             // console.log(result, 'resuyltyBUTTON');
+//             console.log(result,'RESULT IN BUTTONS OF GET');
+//             res.json({ result, pre, nex, nextv, prev })
+//         }).catch(err=> console.log(err));
+// }
 
 exports.deleteExpense = async (req, res, next) => {
     const t = await sequelize.transaction()
@@ -70,7 +122,7 @@ exports.deleteExpense = async (req, res, next) => {
        
         console.log('entry deleted')
     }
-    catch(e){
+    catch(e){p
         await t.rollback()
         console.log('rollback')
         console.log(e)
